ba2f1e5df88cbe6119a72a32aaf4a21214818ce5
Kelvin Kavita <59626906+Kev4HackPro@users.noreply.github.com>
2020-08-06 01:53:49 +0300

    Delete to_be_deleted.py

--- to_be_deleted.py
+++
@@ -1,8 +0,0 @@
-from binance.client import Client
-
-client = Client('FOQB4cg4vMBqFR8qY9fgvf2b8lqxdeBSYYeudYGvThGve6br3VlxrIwcGcfcADfx', 'PakNsgyipkoApcBNZce9i3hcYJvpCkhQ3CwpHgIiZbl1r8nwr6bLW7r4cNXBevF5')
-
-depth = client.get_order_book(symbol='BNBBTC')
-print(depth)
-withdraws = client.get_withdraw_history(asset='BTC')
-print(withdraws)
